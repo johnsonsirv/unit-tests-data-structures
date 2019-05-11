367833724b80307dedbfb3ad0507c6cf5c488459
Victor Johnson <johnsonsirv@gmail.com>
2019-05-11 12:32:22 +0200

    added the queue datastructure, class constructor and push method

--- Queue.js
+++ Queue.js
@@ -1,3 +1,49 @@
 /* Implementing Queue (FIFO -First-In-First-Out 
     Using an Linked List to keep track of the internal workings of the Stack
 */
+class Node{
+    constructor(value){
+        this.value = value;
+        this.next = null;
+    }
+}
+class SinglyLinkedList{
+    constructor(){
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+    push(value){
+        let newNode = new Node(value);
+        if(!this.head){
+            this.head = newNode;
+            this.tail = this.head;
+        }
+        else{
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        this.length++;
+    }
+    shift(){
+        if(!this.head) return undefined;
+        let temp = this.head;
+        this.head = temp.next;
+        this.length--;
+        if(this.length===0){
+            this.tail = null;
+        }
+        return temp.value;
+    }
+
+}
+
+class Queue{
+    constructor(){
+        this.list = new SinglyLinkedList
+    }
+
+    push(value){
+        
+    }
+}
