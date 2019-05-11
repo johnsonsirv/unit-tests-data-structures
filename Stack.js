5eebc18dffd2941f17981f0dc831b9958f5b2c67
Victor Johnson <johnsonsirv@gmail.com>
2019-05-11 11:34:00 +0200

    added the push method to add item to the stack

--- Stack.js
+++ Stack.js
@@ -1,8 +1,55 @@
 // Implementing Stack (LIFO - Last-In-First-Out)
 /* Using an Linked List to keep track of the internal workings of the Stack*/
+class Node{
+    constructor(value){
+        this.value = value;
+        this.next = null;
+    }
+}
+class SinglyLinkedList{
+    constructor(){
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+    push(value){
+        let newNode = new Node(value);
+        if(!this.head){
+            this.head = newNode;
+            this.tail = this.head;
+        }
+        else{
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        this.length++;
+    }
+    pop(){
+        if(!this.head) return undefined;
+        let newTail = this.head;
+        let temp = newTail;
+        while(temp.next!=null){
+            newTail = temp;
+            temp = temp.next
+        }                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
+        this.tail = newTail;
+        this.tail.next= null;
+        this.length--;
+        if(this.length===0){
+            this.head = null;
+            this.tail = null;
+        }
+        return temp;
+    }
+}
 
 class Stack{
     constructor(){
-        
+        this.list = new SinglyLinkedList
     }
+    //Push method, adss an item to the stack
+    push(data){
+        this.list.push(data)
+    }
+
 }
