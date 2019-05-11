ac6263bac1b37ecadea39e8cdae85682050fcd75
Victor Johnson <johnsonsirv@gmail.com>
2019-05-11 11:23:47 +0200

    added the Stack datastructure, created the class constructor

---
+++ Stack.js
@@ -0,0 +1,8 @@
+// Implementing Stack (LIFO - Last-In-First-Out)
+/* Using an Linked List to keep track of the internal workings of the Stack*/
+
+class Stack{
+    constructor(){
+        
+    }
+}
