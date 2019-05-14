bccfcf5e8b404359261ad9e9c88732e2f7f23a45
Victor Johnson <johnsonsirv@gmail.com>
2019-05-14 14:53:12 +0200

    inital commit

---
+++ TowerOfHanoi.js
@@ -0,0 +1,15 @@
+/* 
+    start = 1
+spare = dest - start
+dest =3
+
+n = 2 / size of disks
+move = n-1 from start to spare
+move 1 from start to destination
+mover n-1 from spare to destinaton
+
+*/
+
+function Tower(n, start=1, dest=3, spare = 2){
+    
+}
