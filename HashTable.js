98c8c3d46c6f60cd786519baff4722e0a866f7cf
Victor Johnson <johnsonsirv@gmail.com>
2019-06-14 00:43:10 +0200

    optimised hash function for speed and near constant time

--- HashTable.js
+++ HashTable.js
@@ -1,8 +1,10 @@
 function hash(key, arrayLength) {
   let total = 0
-  for (let char of key) {
+  const WEIRD_PRIME = 0
+  for (let i =0; i < Math.min(key.length, 100); i++) {
+    let char = key[i]
     let value = char.charCodeAt(0) - 96
-    total = (total + value) % arrayLength
+    total = (total * WEIRD_PRIME + value) % arrayLength
   }
   return total
 }
