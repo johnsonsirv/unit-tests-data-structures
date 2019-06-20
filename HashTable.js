6e620d2a07e1a5fb346ca7eae43a8849c52c7ca7
Victor Johnson <johnsonsirv@gmail.com>
2019-06-21 00:03:27 +0200

    added a hashtable data structure class, with a hash function

--- HashTable.js
+++ HashTable.js
@@ -1,15 +1,27 @@
 //prime number as the size of the array
-function hash(key, arrayLength) {
-  let total = 0
-  const WEIRD_PRIME = 0
-  for (let i =0; i < Math.min(key.length, 100); i++) {
-    let char = key[i]
-    let value = char.charCodeAt(0) - 96
-    total = (total * WEIRD_PRIME + value) % arrayLength
-  }
-  return total
-}
+
 
 function hash_function(n) {
   return Math.abs(n) % 11
+}
+
+class HashTable{
+  constructor(size = 53){
+    this.keyMap = new Array(size)
+  }
+
+  hash(key) {
+    let total = 0
+    const WEIRD_PRIME = 31
+    for (let i =0; i < Math.min(key.length, 100); i++) {
+      let char = key[i]
+      let value = char.charCodeAt(0) - 96
+      total = (total * WEIRD_PRIME + value) % this.keyMap.length
+    }
+    return total
+  }
+
+  set(key, value){
+    
+  }
 }
